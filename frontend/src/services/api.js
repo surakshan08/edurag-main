fd0db3a17f1cf85b8ea83aedc3134a0b2c715776
surakshan <s.a.k.smpl@gmail.com>
2026-03-02 18:13:48 +0530

    Final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "https://edurag-main-production.up.railway.app";
 
 export async function sendChatMessage(query) {
-  const response = await fetch(API_URL, {
+  const response = await fetch(`${API_URL}/chat`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
